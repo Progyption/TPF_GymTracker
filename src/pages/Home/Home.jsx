c543801bb407b06c3b75dcf3c67dfb736ab2cb96
Kamil Bednarski <kamil.bednarski.student@gmail.com>
2026-04-28 10:25:58 +0200

    Wstępny zarys strony

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,40 @@
-import styles from './Home.module.css'
+import styles from './Home.module.css';
 
 function Home() {
-  return (
-    <main className={styles.container}>
-      <h1 className={styles.title}>Welcome to TPF</h1>
-      <p className={styles.subtitle}>A collaborative React project by Kamil, Jakub & Mykola</p>
-    </main>
-  )
+    return (
+        <div className={styles.page}>
+
+            {/* HEADER */}
+            <header className={styles.header}>
+                <div className={styles.logo}>Logo</div>
+                <nav className={styles.nav}>
+                    <button>Dashboard</button>
+                    <button>Treningi</button>
+                    <button>Analiza</button>
+                    <button>Profil</button>
+                </nav>
+            </header>
+
+            {/* MAIN */}
+            <main className={styles.main}>
+                <div className={styles.topRow}>
+                    <div className={styles.card}>Zaplanowane treningi</div>
+                    <div className={styles.card}>Wykonane treningi</div>
+                    <div className={styles.card}>Alerty</div>
+                </div>
+                <div className={styles.bottomRow}>
+                    <div className={styles.card}>Alerty o zmianach wyników</div>
+                    <div className={styles.card}>Ostatnie treningi</div>
+                </div>
+            </main>
+
+            {/* FOOTER */}
+            <footer className={styles.footer}>
+                <button className={styles.addButton}>Dodaj trening</button>
+            </footer>
+
+        </div>
+    );
 }
 
-export default Home
+export default Home;
